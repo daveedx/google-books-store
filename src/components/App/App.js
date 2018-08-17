6fa1c42b06cf7f94da953e4ea7a907f428cef293
daveedx <davidbodrogi@kinsta.com>
2018-08-17 15:38:42 +0200

    add page loader as Spin component for later purposes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import {
   Switch,
   withRouter,
 } from 'react-router-dom'
-import { Layout, Menu } from 'antd'
+import { Layout, Menu, Spin } from 'antd'
 import type { Node as ReactNode } from 'react'
 
 import HomePage from '../Pages/HomePage'
@@ -18,31 +18,34 @@ import logo from './books.svg'
 const { Header, Content } = Layout
 
 const App = (): ReactNode => (
-  <Layout className="app-container">
-    <Header className="app-header">
-      <Link to="/">
+  <Spin
+    wrapperClassName="app-loader"
+    spinning={false}
+  >
+    <Layout className="app-container">
+      <Header className="app-header">
         <img src={logo} className="logo" alt="logo" title="Google Books Store" />
-      </Link>
 
-      <Menu
-        className="main-menu"
-        theme="dark"
-        mode="horizontal"
-        selectable={false}
-      >
-        <Menu.Item key="menu-home">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-      </Menu>
-    </Header>
+        <Menu
+          className="main-menu"
+          theme="dark"
+          mode="horizontal"
+          selectable={false}
+        >
+          <Menu.Item key="menu-home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
 
-    <Content className="app-content">
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </Content>
-  </Layout>
+      <Content className="app-content">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Content>
+    </Layout>
+  </Spin>
 )
 
 export default withRouter(App)
