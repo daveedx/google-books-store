50c2603f8277fefc660a44d5472c37f40071b076
daveedx <davidbodrogi@kinsta.com>
2018-08-17 15:45:35 +0200

    configure redux store

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
+import store from './store'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  global.window.document.getElementById('root'),
 )
 registerServiceWorker()
