9831ddb163a26047a62a7e58f9e322df3f0ad04b
daveedx <davidbodrogi@kinsta.com>
2018-08-17 15:33:30 +0200

    add menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,33 @@
 // @flow
 
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 import type { Node as ReactNode } from 'react'
 
 import './App.css'
+import logo from './books.svg'
 
-const { Content } = Layout
+const { Header, Content } = Layout
 
 const App = (): ReactNode => (
   <Layout className="app-container">
+    <Header className="app-header">
+      <a href="/">
+        <img src={logo} className="logo" alt="logo" title="Google Books Store" />
+      </a>
+
+      <Menu
+        className="main-menu"
+        theme="dark"
+        mode="horizontal"
+        selectable={false}
+      >
+        <Menu.Item key="menu-home">
+          <a href="/">Home</a>
+        </Menu.Item>
+      </Menu>
+    </Header>
+
     <Content className="app-content">
       content
     </Content>
