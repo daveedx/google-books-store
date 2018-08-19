b6fa48eead3e408480574c4ed89f8c595e9a30b1
David Bodrogi <dennis001@freemail.hu>
2018-08-20 00:40:55 +0200

    fix styling issues; amend flow type in ppLoaderVisibility.js

--- src/reducers/appLoaderVisibility.js
+++ src/reducers/appLoaderVisibility.js
@@ -2,12 +2,14 @@
 
 import { SHOW_APP_LOADER, HIDE_APP_LOADER } from '../actions'
 
+type State = boolean
+
 type Action = {
   type: string,
 }
 
-const appLoaderVisibility = (state: boolean = false, action: Action) => {
-  switch (action.type) {
+const appLoaderVisibility = (state: State = false, { type: actionType }: Action) => {
+  switch (actionType) {
     case SHOW_APP_LOADER:
       return true
 
